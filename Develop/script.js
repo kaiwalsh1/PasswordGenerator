923e0ba002b10eb42b7695ff7342368c3c425705
kai <kai@kaiconsulting.co>
2021-11-06 09:04:40 -0600

    added if statements and variables

--- Develop/script.js
+++ Develop/script.js
@@ -1,21 +1,108 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-
-var tagName = prompt('How many characters do you want the password to contain?');
-var nextTag = confirm('Click OK to confirm including lowercase characters.');
-var nextTag = confirm('Click OK to confirm including uppercase characters.')
-var nextTag = confirm('Click OK to confirm using numeric characters.');
-var nextTag = confirm('Click OK to confirm using special characters.');
+var lowerCaseArray = [
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+];
+var upperCaseArray = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+];
+var numericArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+var specialArray = [
+  '@',
+  '%',
+  '+',
+  '\\',
+  '/',
+  "'",
+  '!',
+  '#',
+  '$',
+  '^',
+  '?',
+  ':',
+  ',',
+  ')',
+  '(',
+  '}',
+  '{',
+  ']',
+  '[',
+  '~',
+  '-',
+  '_',
+  '.',
+];
+var passwordArray = [];
+// if (nextTag) {
+//   var secondTagName = 
 
 
 // function for button to generate password 
 function generatePassword() {
-  console.log('Hey! You clicked the button');
+  // Prompt user with password criteria
+  var characters = prompt('How many characters do you want the password to contain?');
+    if(characters < 8 || characters > 128) {
+      alert('Character length must be between 8 and 128 characters');
+      return;
+    };
+  var lowercase = confirm('Click OK to confirm including lowercase characters.');
+  var uppercase = confirm('Click OK to confirm including uppercase characters.');
+  var numeric = confirm('Click OK to confirm using numeric characters.');
+  var special = confirm('Click OK to confirm using special characters.');
 
-// Prompt user with password criteria
-const prompter = function() {
+    
+  };
 
-}
 //    Password length 8 < 128
 //    lowercase, uppercase, special characters
 // Validate input
@@ -23,8 +110,6 @@ const prompter = function() {
 // Display password on page 
 
 
-return "Generated pw will go here!";
-}
 
 // Write password to the #password input
 function writePassword() {
